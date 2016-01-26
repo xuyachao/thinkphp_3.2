66b69523cf16ea3d18384de4c765dc0e855e732a
xuyachao <xuyachao258@163.com>
2016-01-26 18:01:26 +0800

    删除一些文件

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -16,46 +16,4 @@ class IndexController extends BasicController {
 		$this->view();
     }
     
-    /**
-     * 菜单列表
-     */
-    public function menuList()
-    {
-    	$menuModel = M('menu');
-    	$data = $menuModel->where(array('pid'=>0))->select();
-    	
-    	$this->assign('data', $data);
-    	$this->view();
-    }
-    
-    /**
-     * 添加菜单
-     */
-    public function addMenu(){
-    	$menuModel = M('menu');
-    	
-    	if (IS_POST)
-    	{
-    		if ($menuModel->create())
-    		{
-    			$id = $menuModel->add();
-    			if ($id)
-    			{
-    				$cache = $this->initRedis();
-    				unset($cache->menu);
-    				$this->success('添加成功');
-    			}
-    			else
-    			{
-    				$this->error('添加失败');
-    			}
-    		}
-    		
-    		exit;
-    	}
-    	
-    	$data = $menuModel->where(array('pid'=>0))->select();
-    	$this->assign('data', $data);
-    	$this->view();
-    }
 }
